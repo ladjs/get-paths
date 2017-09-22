3a94fc384fe4c4f9fe5a7ed0375b339a77818f38
Nick Baugh <niftylettuce@gmail.com>
2017-09-22 04:36:17 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs-extra');
+
+// TODO: this should be a package or we should use one that does same thing
+// <https://github.com/queckezz/koa-views/blob/master/src/index.js>
+function getPaths(abs, rel, ext) {
+  return fs
+    .stat(path.join(abs, rel))
+    .then(stats => {
+      if (stats.isDirectory()) {
+        // a directory
+        return {
+          rel: path.join(rel, `index.${ext}`),
+          ext
+        };
+      }
+      // a file
+      return { rel, ext: path.extname(rel).slice(1) };
+    })
+    .catch(err => {
+      // not a valid file/directory
+      if (!path.extname(rel) || path.extname(rel).slice(1) !== ext) {
+        // Template file has been provided without the right extension
+        // so append to it to try another lookup
+        return getPaths(abs, `${rel}.${ext}`, ext);
+      }
+      throw err;
+    });
+}
+
+module.exports = getPaths;
