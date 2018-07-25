52f338d4d5ef5640657c8c996b4ee72ab6906d0d
Dian Fay <dian.m.fay@gmail.com>
2018-07-24 23:44:54 -0400

    fix: prioritize files over directories BREAKING CHANGE: usage without extensions now returns a file path when a file and directory share names

--- index.js
+++ index.js
@@ -5,24 +5,30 @@ function getPaths(abs, rel, ext) {
   return fs
     .stat(path.join(abs, rel))
     .then(stats => {
-      if (stats.isDirectory()) {
-        // a directory
+      if (stats.isFile()) {
         return {
-          rel: path.join(rel, `index.${ext}`),
+          rel,
           ext
         };
       }
-      // a file
-      return { rel, ext: path.extname(rel).slice(1) };
+
+      return Promise.reject();
     })
     .catch(err => {
-      // not a valid file/directory
       if (!path.extname(rel) || path.extname(rel).slice(1) !== ext) {
-        // Template file has been provided without the right extension
-        // so append to it to try another lookup
         return getPaths(abs, `${rel}.${ext}`, ext);
       }
+
       throw err;
+    })
+    .catch(err => {
+      return fs.stat(path.join(abs, rel)).then(stats => {
+        if (stats.isDirectory()) {
+          return getPaths(abs, `${rel}/index.${ext}`, ext);
+        }
+
+        throw err;
+      });
     });
 }
 
--- test/test.js
+++ test/test.js
@@ -1,7 +1,52 @@
+const path = require('path');
 const test = require('ava');
 
 const getPaths = require('../');
 
+const contentDir = path.resolve(`${__dirname}/content`);
+
 test('returns a function', t => {
   t.true(typeof getPaths === 'function');
+});
+
+test('gets a file', async t => {
+  const val = await getPaths(contentDir, 'alpha', 'pug');
+
+  t.deepEqual(val, { ext: 'pug', rel: 'alpha.pug' });
+});
+
+test('gets a file with explicit extension', async t => {
+  const val = await getPaths(contentDir, 'alpha.pug', 'pug');
+
+  t.deepEqual(val, { ext: 'pug', rel: 'alpha.pug' });
+});
+
+test('gets a file without explicit extension', async t => {
+  const val = await getPaths(contentDir, 'alpha', 'pug');
+
+  t.deepEqual(val, { ext: 'pug', rel: 'alpha.pug' });
+});
+
+test('gets a directory index', async t => {
+  const val = await getPaths(contentDir, 'beta', 'pug');
+
+  t.deepEqual(val, { ext: 'pug', rel: 'beta/index.pug' });
+});
+
+test('prioritizes files over directories', async t => {
+  const val = await getPaths(contentDir, 'gamma', 'pug');
+
+  t.deepEqual(val, { ext: 'pug', rel: 'gamma.pug' });
+});
+
+test('does not get a nonexistent path', async t => {
+  try {
+    await getPaths(contentDir, 'omega', 'pug');
+
+    t.fail();
+  } catch (err) {
+    t.is(err.code, 'ENOENT');
+
+    t.pass();
+  }
 });
